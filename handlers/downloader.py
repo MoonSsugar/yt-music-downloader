b6515a0abdaf94f639abe9e22b728f6421ab1ed9
MoonSsugar <hirnyjt@gmail.com>
2025-05-30 22:21:50 +0300

    a little change

--- handlers/downloader.py
+++ handlers/downloader.py
@@ -12,8 +12,12 @@ downloader_router = Router()
 
 @downloader_router.callback_query(F.data == "music")
 async def get_url(call: CallbackQuery, state: FSMContext):
+    if os.path.exists(f"{call.from_user.id}"):
+        pass
+    else:
+        os.mkdir(f"{call.from_user.id}")
     await state.set_state(UrlState.url)
-    await call.message.answer(text="Wait a minute, your musuc is downloading...")
+    await call.message.answer(text="Send url.")
     await call.answer()
 
 @downloader_router.message(StateFilter(UrlState.url))
@@ -21,11 +25,14 @@ async def downloader(message: Message, state: FSMContext):
     await state.set_data({"url": message.text})
     data = await state.get_data()
     url = data["url"]
-    await download(url=url)
-    for file in os.listdir("."):
+    counter = 0
+    await download(url=url, id=str(message.from_user.id))
+    for file in os.listdir(f"{message.from_user.id}\\"):
         if ".m4a" not in file:
             continue
-        await message.answer_document(FSInputFile(file))
-        os.remove(file)
+        await message.answer_audio(FSInputFile(path=f"{message.from_user.id}\\{file}"))
+        os.remove(f"{message.from_user.id}\\{file}")
+        counter+=1
     await state.clear()
+    await message.answer(text=f"{counter}")
     await message.answer(text="Anything else?", reply_markup=main_menu)
--- service/yt_downloader.py
+++ service/yt_downloader.py
@@ -1,9 +1,15 @@
 from yt_dlp import YoutubeDL
 
-async def download(url: str):
+async def download(url: str, id: str):
     ydl_opts = {
-        "format": "m4a/bestaudio/best"
-    }
+        "format": "m4a/bestaudio/best",
+        "ffmpeg_location": "d:\\bin\\ffmpeg.exe",
+        "outtmpl": id+"\\%(title)s.%(ext)s",
+        'postprocessors': [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'm4a',
+    }]
+}
     ydl = YoutubeDL(ydl_opts)
     with ydl:
         ydl.download(url)
